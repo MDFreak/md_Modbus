1aaac9aaedb8aad74bba075b4ec18210044d73d2
MDFreak <inbox@martin-dorfner.at>
2024-01-18 02:30:34 +0100

    multiple slave first success

--- examples/RS485_HalfDuplex/RS485_HalfDuplex.ino
+++ examples/RS485_HalfDuplex/RS485_HalfDuplex.ino
@@ -1,5 +1,4 @@
 /*
-
   RS485_HalfDuplex.pde - example using ModbusMaster library to communicate
   with EPSolar LS2024B controller using a half-duplex RS485 transceiver.
 
@@ -23,76 +22,59 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-
-*/
-
+ */
 #include <ModbusMaster.h>
-
 /*!
   We're using a MAX485-compatible RS485 Transceiver.
   Rx/Tx is hooked up to the hardware serial port at 'Serial'.
   The Data Enable and Receiver Enable pins are hooked up as follows:
-*/
+ */
 #define MAX485_DE      3
 #define MAX485_RE_NEG  2
-
 // instantiate ModbusMaster object
 ModbusMaster node;
-
 void preTransmission()
-{
-  digitalWrite(MAX485_RE_NEG, 1);
-  digitalWrite(MAX485_DE, 1);
-}
-
-void postTransmission()
-{
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
-}
-
-void setup()
-{
-  pinMode(MAX485_RE_NEG, OUTPUT);
-  pinMode(MAX485_DE, OUTPUT);
-  // Init in receive mode
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
-
-  // Modbus communication runs at 115200 baud
-  Serial.begin(115200);
-
-  // Modbus slave ID 1
-  node.begin(1, Serial);
-  // Callbacks allow us to configure the RS485 transceiver correctly
-  node.preTransmission(preTransmission);
-  node.postTransmission(postTransmission);
-}
-
-bool state = true;
-
-void loop()
-{
-  uint8_t result;
-  uint16_t data[6];
-  
-  // Toggle the coil at address 0x0002 (Manual Load Control)
-  result = node.writeSingleCoil(0x0002, state);
-  state = !state;
-
-  // Read 16 registers starting at 0x3100)
-  result = node.readInputRegisters(0x3100, 16);
-  if (result == node.ku8MBSuccess)
   {
-    Serial.print("Vbatt: ");
-    Serial.println(node.getResponseBuffer(0x04)/100.0f);
-    Serial.print("Vload: ");
-    Serial.println(node.getResponseBuffer(0xC0)/100.0f);
-    Serial.print("Pload: ");
-    Serial.println((node.getResponseBuffer(0x0D) +
-                    node.getResponseBuffer(0x0E) << 16)/100.0f);
+    digitalWrite(MAX485_RE_NEG, 1);
+    digitalWrite(MAX485_DE, 1);
   }
-
-  delay(1000);
-}
+void postTransmission()
+  {
+    digitalWrite(MAX485_RE_NEG, 0);
+    digitalWrite(MAX485_DE, 0);
+  }
+void setup()
+  {
+    pinMode(MAX485_RE_NEG, OUTPUT);
+    pinMode(MAX485_DE, OUTPUT);
+    // Init in receive mode
+    digitalWrite(MAX485_RE_NEG, 0);
+    digitalWrite(MAX485_DE, 0);
+    // Modbus communication runs at 115200 baud
+    Serial.begin(115200);
+    // Modbus slave ID 1
+    node.begin(1, Serial);
+    // Callbacks allow us to configure the RS485 transceiver correctly
+    node.preTransmission(preTransmission);
+    node.postTransmission(postTransmission);
+  }
+bool state = true;
+void loop()
+  {
+    uint8_t result;
+    uint16_t data[6];
+    // Toggle the coil at address 0x0002 (Manual Load Control)
+    result = node.writeSingleCoil(0x0002, state);
+    state = !state;
+    // Read 16 registers starting at 0x3100)
+    result = node.readInputRegisters(0x3100, 16);
+    if (result == node.ku8MBSuccess)
+      {
+        Serial.print("Vbatt: "); Serial.println( node.getResponseBuffer(0x04)/100.0f);
+        Serial.print("Vload: "); Serial.println( node.getResponseBuffer(0xC0)/100.0f);
+        Serial.print("Pload: "); Serial.println((node.getResponseBuffer(0x0D) +
+                                                 node.getResponseBuffer(0x0E) << 16)/100.0f);
+      }
+    delay(1000);
+  }
 
